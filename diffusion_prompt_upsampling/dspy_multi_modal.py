da817d8ef76b79c6c181b4d7f8bede311c25d701
soumik12345 <19soumik.rakshit96@gmail.com>
2024-07-31 15:05:32 +0000

    update: DSPyOpenAIMultiModalLM

--- diffusion_prompt_upsampling/dspy_multi_modal.py
+++ diffusion_prompt_upsampling/dspy_multi_modal.py
@@ -1,10 +1,10 @@
 import os
-import requests
-from typing import Any, Dict, Literal, List
+from typing import Any, Literal
 
 import weave
 from weave.integrations.dspy.dspy_sdk import dspy_wrapper
 from dsp import GPT3
+from openai import OpenAI
 
 from .utils import find_base64_images
 
@@ -13,7 +13,7 @@ class DSPyOpenAIMultiModalLM(GPT3):
 
     def __init__(
         self,
-        model: str = "gpt-3.5-turbo-instruct",
+        model: str = "gpt-4o",
         api_key: str | None = None,
         api_provider: Literal["openai"] = "openai",
         api_base: str | None = None,
@@ -24,6 +24,8 @@ class DSPyOpenAIMultiModalLM(GPT3):
         super().__init__(
             model, api_key, api_provider, api_base, model_type, system_prompt, **kwargs
         )
+        self.model_type = model
+        self._openai_client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
     @weave.op()
     def create_messages(self, prompt: str):
@@ -41,27 +43,11 @@ class DSPyOpenAIMultiModalLM(GPT3):
         return messages
 
     @weave.op()
-    def get_chat_reponse(
-        self, messages: List[Dict[str, Any]], kwargs
-    ) -> Dict[str, Any]:
-        api_key = os.environ.get("OPENAI_API_KEY")
-        return requests.post(
-            "https://api.openai.com/v1/chat/completions",
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": f"Bearer {api_key}",
-            },
-            json={
-                **kwargs,
-                "model": "gpt-4o",
-                "messages": messages,
-            },
-        ).json()
-
-    @weave.op()
     def basic_request(self, prompt: str, **kwargs):
         messages = self.create_messages(prompt)
-        response = self.get_chat_reponse(messages, kwargs)
+        response = self._openai_client.chat.completions.create(
+            model=self.model_type, messages=messages, **kwargs
+        )
         self.history.append({"prompt": prompt, "response": response, "kwargs": kwargs})
         return response
 
@@ -69,14 +55,14 @@ class DSPyOpenAIMultiModalLM(GPT3):
     def request(self, prompt: str, **kwargs):
         return super().request(prompt, **kwargs)
 
-    @dspy_wrapper(
-        name="diffusion_prompt_upsampling.dspy_multi_modal.DSPyOpenAIMultiModalLM"
-    )
+    @dspy_wrapper(name="DSPyOpenAIMultiModalLM")
     def __call__(
-        self,
-        prompt: str,
-        only_completed: bool = True,
-        return_sorted: bool = False,
-        **kwargs,
+        self, prompt: str, only_completed: bool = True, **kwargs
     ) -> list[dict[str, Any]]:
-        return super().__call__(prompt, only_completed, return_sorted, **kwargs)
+        response = self.request(prompt, **kwargs)
+        choices = (
+            [choice for choice in response.choices if choice.finish_reason == "stop"]
+            if only_completed and len(response.choices) != 0
+            else response.choices
+        )
+        return [choice.message.content for choice in choices]
